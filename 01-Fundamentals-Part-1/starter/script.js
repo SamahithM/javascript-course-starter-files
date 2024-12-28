2424192a9adfeddcf224859b1c6df50ada86db59
SamahithM <gowdasamahithm@gmail.com>
2024-12-28 05:27:01 +0000

    day 1 assignment 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,22 @@
-console.log(1 + 1);
+
+// values and variables 
+let country = "IND";
+let continent = "Asia";
+let population = 1.4;
+
+console.log(country, continent, population);
+
+//  data types 
+// strings 
+let _name = "samahith";
+console.log(_name);
+
+// numbers 
+let _num  = 7;
+console.log(_num)
+
+// boolean 
+let _true = true;
+let _false = false;
+console.log(_true, _false)
+
