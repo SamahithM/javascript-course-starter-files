ada8fa6d14be587d2cd3b96e230b2592e92ab078
SamahithM <gowdasamahithm@gmail.com>
2024-12-28 07:30:48 +0000

    ntng

---
+++ 01-Fundamentals-Part-1/starter/switch.js
@@ -0,0 +1,27 @@
+const day = prompt("Enter the day of the week: ");
+switch (day) {
+    case "monday":
+        console.log("monday");
+        break;
+    case "tuesday":
+        console.log("tuesday");
+        break;
+    case "wednesday":
+        console.log("wednesday");
+        break;
+    case "thursday":
+        console.log("thursday");
+        break;
+    case "friday":
+        console.log("friday");
+        break;
+    case "saturday":
+        console.log("saturday");
+        break;
+    case "sunday":
+        console.log("sunday");
+        break;
+    default:
+        console.log("Not a valid day");
+        break;
+}
