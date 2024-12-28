305f32b1d33d6a39fa859d1d137a8ad80b1333ba
SamahithM <gowdasamahithm@gmail.com>
2024-12-28 06:38:23 +0000

    ng

--- 01-Fundamentals-Part-1/starter/valuetypesAndCoercion.js
+++ 01-Fundamentals-Part-1/starter/valuetypesAndCoercion.js
@@ -9,3 +9,23 @@ console.log(Number(inputYear))
 // js changes types in backend 
 
 console.log("i am " + 23 + "years old ")
+console.log("27" * 23 )
+console.log("23" / "2")
+console.log("23" > "18")
+
+//etc 
+
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0))
+console.log(Boolean(undefined))
+console.log(Boolean('Jonas'))
+console.log(Boolean({}))
+console.log(Boolean(''))
+
+const money = 0
+if(money){
+    console.log("dont spend it all")         
+}
+else{   
+    console.log("you should get a job")
+}       
