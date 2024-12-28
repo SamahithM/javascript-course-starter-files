766369ae58dc8d3eda24eacbb825e6d0efd7034c
SamahithM <gowdasamahithm@gmail.com>
2024-12-28 05:40:49 +0000

    operators mod 1

---
+++ 01-Fundamentals-Part-1/starter/oprators.js
@@ -0,0 +1,40 @@
+// operators 
+
+// 1. Arithmetic operators
+let add = 5 + 5;
+let sub = 5 - 5;
+let mul = 5 * 5;
+let div = 5 / 5;
+let mod = 5 % 5;
+console.log(add, sub, mul, div, mod);
+
+// 2. Comparison operators
+let greater = 5 > 5;
+let greaterOrEqual = 5 >= 5;
+let less = 5 < 5;
+let lessOrEqual = 5 <= 5;
+let equal = 5 == 5;
+let strictEqual = 5 === 5;
+let notEqual = 5 != 5;
+let strictNotEqual = 5 !== 5;
+console.log(greater, greaterOrEqual, less, lessOrEqual, equal, strictEqual, notEqual, strictNotEqual);
+
+// 3. Logical operators
+let and = true && true;
+let or = true || false;
+let not = !true;
+console.log(and, or, not);
+
+// 4. Assignment operators
+let x = 5;
+console.log(x);
+x += 5;
+console.log(x);
+x -= 5;
+console.log(x);
+x *= 5;
+console.log(x);
+x /= 5;
+console.log(x);
+x %= 5;
+console.log(x);
