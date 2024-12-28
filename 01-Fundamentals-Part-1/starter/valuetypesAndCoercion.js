417441017219ad5d3791eeb9b72cf5b714785c55
SamahithM <gowdasamahithm@gmail.com>
2024-12-28 06:29:56 +0000

    ng

---
+++ 01-Fundamentals-Part-1/starter/valuetypesAndCoercion.js
@@ -0,0 +1,11 @@
+// type conversion 
+// manual type changes
+
+let inputYear = "1991"
+console.log(Number(inputYear))
+
+
+//type coersion 
+// js changes types in backend 
+
+console.log("i am " + 23 + "years old ")
